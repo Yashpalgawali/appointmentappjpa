1ed3b978170e4811ddc2e9ee20a53463c43ffe5d
Yashpal Gawali <gawaliyashpal@gmail.com>
2026-05-29 17:42:22 +0530

    29 May sec commit

--- src/main/java/com/example/demo/dto/ResponseDto.java
+++ src/main/java/com/example/demo/dto/ResponseDto.java
@@ -1,8 +1,14 @@
 package com.example.demo.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class ResponseDto {
 
 	private String statusMessage;
