6e0f975cd8d2da5b66817c511695c2215b12d32b
Yashpal Gawali <gawaliyashpal@gmail.com>
2026-05-30 12:30:55 +0530

    Designation controller is added

--- src/main/java/com/example/demo/service/impl/DesignationServImpl.java
+++ src/main/java/com/example/demo/service/impl/DesignationServImpl.java
@@ -1,43 +1,58 @@
 package com.example.demo.service.impl;
 
 import java.util.List;
+import java.util.function.Consumer;
 
 import org.springframework.stereotype.Service;
 
-import com.example.demo.entities.Company;
-import com.example.demo.repository.CompanyRepository;
-import com.example.demo.service.ICompanyService;
+import com.example.demo.entities.Designation;
+import com.example.demo.exceptions.GlobalException;
+import com.example.demo.exceptions.ResourceAlreadyExistsException;
+import com.example.demo.exceptions.ResourceNotExistsException;
+import com.example.demo.repository.DesignationRepository;
+import com.example.demo.service.IDesignationService;
 
 import lombok.RequiredArgsConstructor;
 
-@Service("compserv")
+@Service("desigserv")
 @RequiredArgsConstructor
-public class DesignationServImpl implements ICompanyService {
+public class DesignationServImpl implements IDesignationService {
 
-	private final CompanyRepository comprepo;
+	private final DesignationRepository desigrepo;
 
 	@Override
-	public void createCompany(Company company) {
-		comprepo.save(company);
-
+	public void createDesignation(Designation designation) {
+	 
+		String trimmedDesigName = designation.getDesignationName().trim();
+		
+		Consumer<? super Designation> action = d ->  new ResourceAlreadyExistsException("Designation "+trimmedDesigName+" is already present");
+		desigrepo.findByDesignationName(trimmedDesigName).ifPresent(action);
+		
+		designation.setDesignationName(trimmedDesigName);
+		Designation savedDesig = desigrepo.save(designation);
+		if(savedDesig == null ) {
+			throw new GlobalException("Designation "+trimmedDesigName+" is not saved");
+		}
 	}
 
 	@Override
-	public Company getCompanyById(Long id) {
+	public Designation getDesignationById(Long id) {
 		// TODO Auto-generated method stub
+		return desigrepo.findById(id).orElseThrow(() -> new ResourceNotExistsException("No Designation foudn for given ID "+id));
+	}
+
+	@Override
+	public List<Designation> getAllDesignations() {
+		// 
 		return null;
 	}
 
 	@Override
-	public List<Company> getAllCompanies() {
+	public void updateDesignation(Designation designation) {
 		// TODO Auto-generated method stub
-		return null;
+		
 	}
 
-	@Override
-	public void updateCompany(Company company) {
-		// TODO Auto-generated method stub
-
-	}
+	 
 
 }
