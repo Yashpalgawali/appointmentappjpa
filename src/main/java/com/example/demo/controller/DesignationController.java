9be433d5af243629d27238e170822ea88f9e2bbe
Yashpal Gawali <gawaliyashpal@gmail.com>
2026-05-30 13:48:47 +0530

    Designation controller is modified

--- src/main/java/com/example/demo/controller/DesignationController.java
+++ src/main/java/com/example/demo/controller/DesignationController.java
@@ -1,5 +1,7 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,23 +24,31 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("designation")
 public class DesignationController {
 
-	private final IDesignationService  desigserv;
-	
+	private final IDesignationService desigserv;
+
 	@PostMapping("/")
 	public ResponseEntity<ResponseDto> createDesignation(@Valid @RequestBody Designation designation) {
 		desigserv.createDesignation(designation);
-		return ResponseEntity.status(HttpStatus.CREATED).body(new ResponseDto(designation.getDesignationName()+" is created successfully",HttpStatus.CREATED.toString()));
+		return ResponseEntity.status(HttpStatus.CREATED).body(new ResponseDto(
+				designation.getDesignationName() + " is created successfully", HttpStatus.CREATED.toString()));
 	}
-	
+
 	@PutMapping("/")
 	public ResponseEntity<ResponseDto> updateDesignation(@Valid @RequestBody Designation designation) {
 		desigserv.updateDesignation(designation);
-		return ResponseEntity.status(HttpStatus.CREATED).body(new ResponseDto(designation.getDesignationName()+" is updated successfully",HttpStatus.CREATED.toString()));
+		return ResponseEntity.status(HttpStatus.CREATED).body(new ResponseDto(
+				designation.getDesignationName() + " is updated successfully", HttpStatus.CREATED.toString()));
 	}
-	
+
 	@GetMapping("/{id}")
 	public ResponseEntity<Designation> getDesignationById(@PathVariable Long id) {
-		Designation designation= desigserv.getDesignationById(id);
-		return ResponseEntity.status(HttpStatus.OK) .body(designation);
+		Designation designation = desigserv.getDesignationById(id);
+		return ResponseEntity.status(HttpStatus.OK).body(designation);
+	}
+
+	@GetMapping("/")
+	public ResponseEntity<List<Designation>> getAllDesignation() {
+		List<Designation> designationList = desigserv.getAllDesignations();
+		return ResponseEntity.status(HttpStatus.OK).body(designationList);
 	}
 }
--- src/main/java/com/example/demo/service/impl/DesignationServImpl.java
+++ src/main/java/com/example/demo/service/impl/DesignationServImpl.java
@@ -9,6 +9,7 @@ import com.example.demo.entities.Designation;
 import com.example.demo.exceptions.GlobalException;
 import com.example.demo.exceptions.ResourceAlreadyExistsException;
 import com.example.demo.exceptions.ResourceNotExistsException;
+import com.example.demo.exceptions.ResourceNotModifiedException;
 import com.example.demo.repository.DesignationRepository;
 import com.example.demo.service.IDesignationService;
 
@@ -22,37 +23,47 @@ public class DesignationServImpl implements IDesignationService {
 
 	@Override
 	public void createDesignation(Designation designation) {
-	 
+
 		String trimmedDesigName = designation.getDesignationName().trim();
-		
-		Consumer<? super Designation> action = d ->  new ResourceAlreadyExistsException("Designation "+trimmedDesigName+" is already present");
+
+		Consumer<? super Designation> action = d -> new ResourceAlreadyExistsException(
+				"Designation " + trimmedDesigName + " is already present");
 		desigrepo.findByDesignationName(trimmedDesigName).ifPresent(action);
-		
+
 		designation.setDesignationName(trimmedDesigName);
 		Designation savedDesig = desigrepo.save(designation);
-		if(savedDesig == null ) {
-			throw new GlobalException("Designation "+trimmedDesigName+" is not saved");
+		if (savedDesig == null) {
+			throw new GlobalException("Designation " + trimmedDesigName + " is not saved");
 		}
 	}
 
 	@Override
 	public Designation getDesignationById(Long id) {
 		// TODO Auto-generated method stub
-		return desigrepo.findById(id).orElseThrow(() -> new ResourceNotExistsException("No Designation foudn for given ID "+id));
+		return desigrepo.findById(id)
+				.orElseThrow(() -> new ResourceNotExistsException("No Designation foudn for given ID " + id));
 	}
 
 	@Override
 	public List<Designation> getAllDesignations() {
-		// 
-		return null;
+
+		var desigList = desigrepo.findAll();
+		if (desigList.size() > 0)
+			return desigList;
+		throw new ResourceNotExistsException("No Designation(s) found");
 	}
 
 	@Override
 	public void updateDesignation(Designation designation) {
-		// TODO Auto-generated method stub
-		
-	}
 
-	 
+		this.getDesignationById(designation.getDesignationId());
+
+		int result = desigrepo.updateDesignation(designation.getDesignationId(), designation.getDesignationName());
+		if (result < 0) {
+			throw new ResourceNotModifiedException(
+					"Designation " + designation.getDesignationName() + " is not updated");
+		}
+
+	}
 
 }
